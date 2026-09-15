612d02780414bb6438993d482996f9c4c2843c1a
FedericoLazarte <fedelazarte2015@gmail.com>
2026-09-15 17:25:11 -0300

    Agregando Componente Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/layout/footer/Footer";
 import Header from "./components/layout/header/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
